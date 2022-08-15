4274872e76e70ecada4eb577e2937c4bcae04e0d
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-08-15 15:46:52 -0300

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstPWA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/ServiceWorker.js
@@ -0,0 +1,28 @@
+﻿var static_files = [
+    '/css/menu.css',
+    '/FirstPWA.styles.css',
+    '/lib/bootstrap/dist/css/bootstrap.min.css',
+    '/lib/jquery/dist/jquery.min.js',
+    '/lib/bootstrap/dist/js/bootstrap.bundle.min.js',
+    '/js/menu.js',
+    
+    '/favicon.ico'.
+,
+    '/'
+]
+
+
+self.addEventListener("install", event => {
+    console.log("install ...")
+})
+
+self.addEventListener("activate", event => {
+    console.log("activate ...")
+    //! solo se necesita un refresh en vez de dos para ver cambios en el service worker
+    event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener("fetch", event => {
+    console.log(event.request.url)
+    event.respondWith(fetch(event.request.url))
+})
